f0df1c5a08329d8041b1e29b560ff190cad19e46
zhangdongyang <zhangdongyang@joyy.com>
2022-09-22 19:28:17 +0800

    feat: bf

---
+++ spa/backend/index.js
@@ -0,0 +1,15 @@
+const http = require('http');
+http.createServer((req, res) => {
+  console.log(req.method, req.url);
+
+  if (req.url === '/redirect') {
+    res.writeHead(302, {
+      location: 'http://localhost:8848/done',
+    });
+  } else {
+    res.writeHead(200);
+    res.write('hello');
+  }
+
+  res.end();
+}).listen(8848);
