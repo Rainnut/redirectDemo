37dfc1edcce8ef60bbbd8d753538284325e49759
zhangdongyang <zhangdongyang@joyy.com>
2022-09-23 10:33:01 +0800

    feat: ok

--- spa/backend/index.js
+++ spa/backend/index.js
@@ -1,10 +1,11 @@
 const Koa = require("koa");
 const app = new Koa();
 const Router = require("koa-router");
+const cors = require("koa-cors");
 const router = new Router();
 
-router.get('/', async ctx => {
-  const name = ctx.query.name || 'stranger';
+router.get("/", async (ctx) => {
+  const name = ctx.query.name || "stranger";
   ctx.body = {
     message: `Hello, ${name}!`,
   };
@@ -19,8 +20,10 @@ router.get("/main", async (ctx) => {
 
 router.get("/redirect", async (ctx) => {
   const name = ctx.query.name || "main";
-  ctx.redirect('/main')
+  ctx.redirect("http://localhost:8849/otherPage.html");
 });
 
-app.use(router.routes()).listen(8848);
+app.use(cors({
+  origin: '*'
+})).use(router.routes()).listen(8848);
 // https://blog.lishunyang.com/2020/06/redirect.html
